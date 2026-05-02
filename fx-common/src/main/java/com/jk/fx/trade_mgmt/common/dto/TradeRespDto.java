380cadddb0d83edbd78f1c73981c649c0f860859
Kishore Veleti <6062900+javakishore-veleti@users.noreply.github.com>
2026-05-02 09:01:31 -0400

    Add TradeRespDto

---
+++ fx-common/src/main/java/com/jk/fx/trade_mgmt/common/dto/TradeRespDto.java
@@ -0,0 +1,11 @@
+package com.jk.fx.trade_mgmt.common.dto;
+
+import java.math.BigDecimal;
+
+public class TradeRespDto {
+    private String tradeId;
+    private String status;
+    private BigDecimal totalCost;
+
+    // Getters and Setters
+}
