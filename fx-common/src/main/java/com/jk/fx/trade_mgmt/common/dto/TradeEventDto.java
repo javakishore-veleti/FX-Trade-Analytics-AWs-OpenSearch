aef32b59689d2ad354e113fc7e1ddc99f1eb914b
Kishore Veleti <6062900+javakishore-veleti@users.noreply.github.com>
2026-05-02 09:01:56 -0400

    Add TradeEventDto

---
+++ fx-common/src/main/java/com/jk/fx/trade_mgmt/common/dto/TradeEventDto.java
@@ -0,0 +1,10 @@
+package com.jk.fx.trade_mgmt.common.dto;
+
+public class TradeEventDto {
+    private String tradeId;
+    private String region;
+    private String eventType;
+    private TradeReqDto trade;
+
+    // Getters and Setters
+}
